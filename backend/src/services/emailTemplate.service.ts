390e142800620f8e8196526b7f72d52c6b84b32f
rudra-kandel <rudra.kandel00@gmail.com>
2024-08-11 08:58:05 +0545

    fix: send email when user registers

--- backend/src/services/emailTemplate.service.ts
+++ backend/src/services/emailTemplate.service.ts
@@ -1,9 +1,29 @@
-import { publishToQueue } from "@queues/email.queue";
+import EmailTemplate from "@models/EmailTemplate";
+import { emailQueue } from "@queues/email.queue";
 
-export const sendBulkEmails = async (templateId: string, userList: any[], res: Response) => {
-    userList.forEach(async (user) => {
-        await publishToQueue('emailQueue', { templateId, email: user.email });
-    });
-    // sendSuccessResponse(res, 202, 'Emails are being processed in the background');
-};
+// export const sendBulkEmails = async (templateId: string, userList: string[]) => {
+//     userList.forEach(async (user) => {
+//         await emailQueue('emailQueue', { templateId, email: user.email });
+//     });
+//     // sendSuccessResponse(res, 202, 'Emails are being processed in the background');
+// };
 
+export const getOneTemplateById = async (templateId: string) => {
+    const template = await EmailTemplate.findByPk(templateId);
+    if (!template) {
+        throw new Error('Template not found');
+    }
+
+    return template
+}
+
+export const getOneTemplateByName = async (templateName: string) => {
+    const template = await EmailTemplate.findOne({
+        where: { name: templateName }
+    })
+    if (!template) {
+        throw new Error('Template not found');
+    }
+
+    return template;
+}
