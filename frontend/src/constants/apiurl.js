a814287c39ee7b3473cd3c6846d4d8614d887bea
rudra-kandel <rudra.kandel00@gmail.com>
2024-08-11 10:18:07 +0545

    feat: view all logs

--- frontend/src/constants/apiurl.js
+++ frontend/src/constants/apiurl.js
@@ -1,3 +1,4 @@
-export const URL = import.meta.env.API_URL ?? "https://936c-103-156-26-53.ngrok-free.app/";
+export const URL =
+  import.meta.env.API_URL ?? "https://936c-103-156-26-53.ngrok-free.app/";
 
-// export default URL;
+export default URL;
